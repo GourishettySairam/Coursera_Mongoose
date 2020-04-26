0bc03f5a9417d5d1b17d23bc0bddca99688c0d63
Gourishetty Sairam <sairamgourishetty99@gmail.com>
2020-04-26 16:06:49 +0530

    Mongoose Part 3

--- index.js
+++ index.js
@@ -15,11 +15,27 @@ connect.then((db) => {
     })
     .then((dish) => {
             console.log(dish);
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id,
+              {
+                $set: { description: 'Updated test'}
+              },
+              {
+                new: true
+              })
+              .exec();
         })
-        .then((dishes) => {
+        .then((dish) => {
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+        return dish.save();
+    })
+        .then((dish) => {
             console.log("removing dishes");
-            console.log(dishes);
+            console.log(dish);
+            console.log(dish.comments);
             return Dishes.remove({});
         })
         .then(() => {
